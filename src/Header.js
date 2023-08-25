95cb79636e1fc4ed7d75361de3b07ae8882b137e
saremipegah <saremipegah@gmail.com>
2023-08-25 12:01:20 +0330

    1.1

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+const Header = (props) => {
+    // const course = 'Half Stack application development'
+
+    return(
+        <div>
+            <h1>{props.name}</h1>
+        </div>
+    )
+}
+export default Header
